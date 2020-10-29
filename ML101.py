46f2b368fa8bc476167d9e684faed6579add22b5
Marechal <s.f.c.marechal@student.tue.nl>
2020-10-29 10:56:58 +0100

    Second upload

---
+++ ML101.py
@@ -0,0 +1,9 @@
+# # What to do?
+# 1 - Import data
+# 2 - Clean the data - make sure organization/structure is okay (parsing)
+# 3 - Split data - Training set / test set
+# 4 - Create a model (import, training)
+# 5 - Check the output
+# 6 - improve
+
+# # Tools for use
